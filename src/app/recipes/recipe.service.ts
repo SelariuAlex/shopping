07f5e5bd48e8cf01c4daecce2d57d9fd717c4d94
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-30 17:30:23 +0300

    passing ingredients from recipes to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,24 +1,35 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38014.jpg'
+      'Salami Pizza',
+      'Salammi',
+      'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38014.jpg',
+      [new Ingredient('Tomatoes', 2), new Ingredient('Salami', 8)]
     ),
     new Recipe(
-      'A Test',
-      'Test',
-      'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38014.jpg'
+      'Mushrooms Pizza',
+      'Mushrooms',
+      'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38014.jpg',
+      [new Ingredient('Tomatoes', 1), new Ingredient('Mushrooms', 3)]
     )
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
