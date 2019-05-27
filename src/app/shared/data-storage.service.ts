0b4b8a8f79b6d6835b9a1595aa5bd7ebac26d70f
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-27 16:35:23 +0300

    sending PUT requests to save data

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    return this.http.put(
+      'https://shopping-recipe-a8caa.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes()
+    );
+  }
+}
